7011cbdb89c34807585b102123cf7ee978384310
Mahesh Manandhar <manandhar533@gmail.com>
2024-09-07 21:55:03 +0545

    changes in update methods

--- database/migrations/2024_08_10_105738_create_payments_table.php
+++ database/migrations/2024_08_10_105738_create_payments_table.php
@@ -14,9 +14,10 @@ return new class extends Migration
         Schema::create('payments', function (Blueprint $table) {
             $table->id('paymentId');
             $table->string('source');
-            $table->decimal('amount', 8, 2);
-            $table->decimal('installment')->nullable();
-            $table->decimal('paid')->nullable();
+            $table->integer('amount');
+            // $table->decimal('installment')->nullable();
+            $table->integer('paid')->nullable();
+            $table->integer('due')->nullable();
             $table->timestamps();
             $table->softDeletes();
         });
