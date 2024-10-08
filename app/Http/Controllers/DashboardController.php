2b54a8a9725f0120414ca5df7219de16e379d2d6
ecv-rijen <rijen.maharjan@classifiedsventures.com>
2024-10-08 16:13:39 +0545

    feat: add other fields into dashboard controller

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Course;
 use App\Models\Job;
 use App\Models\Payment;
 use App\Models\User;
@@ -14,15 +15,24 @@ class DashboardController extends Controller
     public function getDashboardAnalytics()
     {
         $staffCount = User::whereIn('role', ['accountant', 'admin', 'mentor'])->count();
-        $studentCount = User::whereIn('role', ['student'])->count();
-        $internshipJobCount = Job::count();
-        $totalCreditAmount = Payment::where('type', 'credit')->sum('amount');
 
+        $studentCount = User::whereIn('role', ['student'])->count();
+        $todayStudentCount = User::whereIn('role', ['student'])->whereDate('created_at', today())->count();
+
+        $internshipJobCount = Job::count();
+        $todaysInternshipJobCount = Job::whereDate('created_at', today())->count();
+
+        $totalCourseCount = Course::count();
+
+        $totalCreditAmount = Payment::where('type', 'credit')->sum('amount');
 
         return response()->json([
             'staffCount' => $staffCount,
             'studentCount' => $studentCount,
+            'todayStudentCount' => $todayStudentCount,
             'internshipJobCount' => $internshipJobCount,
+            'todaysInternshipJobCount' => $todaysInternshipJobCount,
+            'totalCourseCount' => $totalCourseCount,
             'totalCreditAmount' => $totalCreditAmount,
         ]);
     }
